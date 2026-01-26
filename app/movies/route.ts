b219e01a58c92b31280cc5cbd90e85cddce6e041
Esther Okiki <estherokiki15@gmail.com>
2026-01-26 20:12:01 +0100

    http POST method

--- app/movies/route.ts
+++ app/movies/route.ts
@@ -7,3 +7,18 @@ import { movies } from "./db";
 export async function GET() {
   return Response.json(movies);
 }
+
+export async function POST(res: Request) {
+  const movie = await res.json();
+  // the movie posted is called
+
+  // console.log("........................", movie);
+
+  const newMovie = { ...movie };
+  // the movie object or objects posted is spread and stored into newMovie
+
+  movies.push(newMovie);
+  // our posted movie object or objects is pushed into the movies array.
+
+  return new Response(JSON.stringify(newMovie));
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Home() {
   return <div className="text-red-700 text-3xl">Home</div>;
 }
