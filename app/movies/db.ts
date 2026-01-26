01fe04e08c42da60cfd5aea08640a9227cc1d595
Esther Okiki <estherokiki15@gmail.com>
2026-01-26 19:25:24 +0100

    http GET method

---
+++ app/movies/db.ts
@@ -0,0 +1,19 @@
+interface Movie {
+  id: number;
+  name: string;
+  ratings: number;
+  genre: string;
+}
+
+export const movies: Movie[] = [
+  { id: 1, name: "The Shawshank Redemption", ratings: 9.3, genre: "Drama" },
+  { id: 2, name: "The Dark Knight", ratings: 9.0, genre: "Action" },
+  { id: 3, name: "Inception", ratings: 8.8, genre: "Sci-Fi" },
+  { id: 4, name: "Pulp Fiction", ratings: 8.9, genre: "Crime" },
+  { id: 5, name: "The Matrix", ratings: 8.7, genre: "Sci-Fi" },
+  { id: 6, name: "Forrest Gump", ratings: 8.8, genre: "Drama" },
+  { id: 7, name: "Interstellar", ratings: 8.6, genre: "Sci-Fi" },
+  { id: 8, name: "The Godfather", ratings: 9.2, genre: "Crime" },
+  { id: 9, name: "Gladiator", ratings: 8.5, genre: "Action" },
+  { id: 10, name: "The Avengers", ratings: 8.0, genre: "Action" },
+];
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="text-red-700 text-3xl">Home</div>;
+}
+
+export default Home;
