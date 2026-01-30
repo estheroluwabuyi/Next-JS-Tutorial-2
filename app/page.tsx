3f73abe55908908ef52ae9afa548dabd775ee4c2
Esther Okiki <estherokiki15@gmail.com>
2026-01-30 19:00:25 +0100

    fonts in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { Bebas_Neue } from "next/font/google";
+
+const bebasFont = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 function Home() {
-  return <div className="text-red-700 text-3xl">Home</div>;
+  return (
+    <div className="text-black bg-white h-screen text-3xl">
+      <h1>Bebas Neue</h1>
+      <p className={`${bebasFont.className} uppercase`}>
+        This is some random texts.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
